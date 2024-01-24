f7197aca93062092c76dd8726cc7db5aeebcf5ef
CtrlAltDrink <83370694+CtrlAltDrink@users.noreply.github.com>
2024-01-23 22:20:18 -0800

    Update get-encodedImageForWeb.ps1

---
+++ get-encodedImageForWeb.ps1
@@ -0,0 +1,9 @@
+# This script takes an image file path as a parameter and returns a base 64 encoded string
+Param ([String]$imagePath)
+# Read the image file as a byte array
+$bytes = Get-Content -Path $imagePath -Encoding Byte
+# Convert the byte array to a base 64 string
+$base64 = [Convert]::ToBase64String($bytes)
+# Return the base 64 string
+return $base64
+"<img src="data:image/png;base64, $base64" alt="Image">" | set-clipboard
