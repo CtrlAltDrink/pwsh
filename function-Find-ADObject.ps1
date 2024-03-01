8791e7314d0418c80b8282b0b6a1812cac72885f
CtrlAltDrink <83370694+CtrlAltDrink@users.noreply.github.com>
2024-02-29 16:08:52 -0800

    Update function-Find-ADObject.ps1

--- function-Find-ADObject.ps1
+++ function-Find-ADObject.ps1
@@ -1,19 +1,20 @@
 function Find-ADobject {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory)][string]$SearchFor
-    )
-    $i = 1
-    Get-ADObject -Filter "Name -like '*$SearchFor*' -or SamAccountName -like '*$SearchFor*'" -Properties SamAccountName,Description,CanonicalName,DistinguishedName | Sort-Object ObjectClass,CanonicalName | Foreach-object {
-        $Object = $_ | Select-Object num,OC,Name,SamAccountName,CanonicalName,Description
-        $Object.CanonicalName = $Object.CanonicalName -replace '.stanislaus.ca.us','' -replace '/',' ▶ '
-        $Object.num = $i
-        
-        If ($_.ObjectClass -eq "user") {$Object.OC = "👤";$Object}
-        If ($_.ObjectClass -eq "group") {$Object.OC = "👥";$Object}
-        #If ($_.ObjectClass -eq "dnsNode") {$Object.OC = $_.ObjectClass;$Object}
-        If ($_.ObjectClass -eq "computer") {$Object.OC = "💻";$Object}
-        
-        $i++
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory)][string]$SearchFor
+	)
+	$i = 1
+	Write-Host "Key: 💻 - Computers || 🕋 - DNSNODE || 👥 - Groups || 👤 - Users" -ForegroundColor Green
+	Get-ADObject -Filter "Name -like '*$SearchFor*' -or SamAccountName -like '*$SearchFor*' -or Mail -like '*$SearchFor*'" -Properties SamAccountName,Description,CanonicalName,DistinguishedName,Mail | Sort-Object ObjectClass,CanonicalName | Foreach-object {
+		$Object = $_ | Select-Object num,OC,Name,SamAccountName,Mail,CanonicalName,Description
+		$Object.CanonicalName = $Object.CanonicalName -replace '.stanislaus.ca.us','' -replace '/',' ▶ '
+		$Object.num = $i
+		
+		If ($_.ObjectClass -eq "user") {$Object.OC = " 👤 ";$Object}
+		If ($_.ObjectClass -eq "group") {$Object.OC = "👥👥";$Object}
+		If ($_.ObjectClass -eq "dnsNode") {$Object.OC = " 🕋 ";$Object}
+		If ($_.ObjectClass -eq "computer") {$Object.OC = " 💻 ";$Object}
+		
+		$i++
     } | FT
 }
