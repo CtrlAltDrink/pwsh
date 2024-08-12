5c240eed7871ffd1ca9cfdfb5faa156bc6dbb5aa
CtrlAltDrink <83370694+CtrlAltDrink@users.noreply.github.com>
2024-08-12 08:58:40 -0700

    Update function-Find-ADObject.ps1

--- function-Find-ADObject.ps1
+++ function-Find-ADObject.ps1
@@ -3,11 +3,12 @@ function Find-ADobject {
 	param (
 		[Parameter(Mandatory)][string]$SearchFor
 	)
+	$DomainNameFilter = ""
 	$i = 1
 	Write-Host "Key: 💻 - Computers || 🕋 - DNSNODE || 👥 - Groups || 👤 - Users" -ForegroundColor Green
 	Get-ADObject -Filter "Name -like '*$SearchFor*' -or SamAccountName -like '*$SearchFor*' -or Mail -like '*$SearchFor*'" -Properties SamAccountName,Description,CanonicalName,DistinguishedName,Mail | Sort-Object ObjectClass,CanonicalName | Foreach-object {
 		$Object = $_ | Select-Object num,OC,Name,SamAccountName,Mail,CanonicalName,Description
-		$Object.CanonicalName = $Object.CanonicalName -replace '.stanislaus.ca.us','' -replace '/',' ▶ '
+		$Object.CanonicalName = $Object.CanonicalName -replace '$DomainNameFilter','' -replace '/',' ▶ '
 		$Object.num = $i
 		
 		If ($_.ObjectClass -eq "user") {$Object.OC = " 👤 ";$Object}
